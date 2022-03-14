73135c427ee728601f1123f9136fa6f2e2750901
Kabao Thao <KabaoKThao@gillettechildrens.com>
2022-03-14 13:57:10 -0500

    added notes

--- ListOfPlayers/Program.cs
+++ ListOfPlayers/Program.cs
@@ -9,7 +9,40 @@ namespace MyApp // Note: actual namespace depends on the project name.
     {
         static void Main(string[] args)
         {
-            
+            List<Player> players = new List<Player>();
+
+            Player player1 = new Player("Chad");
+            Player player2 = new Player("Steve");
+            Player player3 = new Player("Bao");
+
+            players.Add(player1);
+            players.Add(player2);
+            players.Add(player3);
+
+            foreach (Player player in players)
+            {
+                Console.WriteLine(player);
+            }
+
+
+
+            Console.ReadKey();
+        }
+    }
+
+
+    class Player
+    {
+        public String username;
+
+        public Player(String username)
+        {
+            this.username = username;
+        }
+
+        public override string ToString()
+        {
+            return username;
         }
     }
 }
